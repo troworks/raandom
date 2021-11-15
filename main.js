114f1936bd9dcf67a484b5a4ae07ec3152d2884f
troworks <93033092+troworks@users.noreply.github.com>
2021-11-15 11:07:46 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let guy ={
+        name:   "troworks",
+        age: 11,
+        print: "dear mr monster i hope your not fine remember me? yes its your old friend aeb dont ask to mush questions kay?",
+        print2: "aeb im sorry i didn't mean to..",
+        aaaaaa: 'i said be quiet',
+        dogshit: 'im sorry i just wanted to apologize',
+        PU_TUHS:'apologize?',
+        hahahah:'please',
+        HAHAhahaha:'oh you did apologize and what will happen next? take me to your house and after some days treat me like some piece of shit?',
+        HAHAHAHAHA: 'no i wouldnt do that to my own so..',
+        HAHAHAHAHAHAHHAHAHAHHAHAHHA:'now your are my father now but not for so long . yes you treat me like your son now but when i was a little boy you treat me like NO ONE'
+};
+
+console.log(guy)
