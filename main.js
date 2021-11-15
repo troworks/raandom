c726aeddefa2583e4c59e3105a0c8faee3c44686
troworks <93033092+troworks@users.noreply.github.com>
2021-11-15 21:11:44 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -13,3 +13,30 @@ let guy ={
 };
 
 console.log(guy)
+
+var sites = [
+        'https://youtu.be/1TgdJZ7zCSU',
+        'https://youtu.be/x4SsMtmcf4w',
+        'https://youtu.be/I6rKVVL1KEI',
+        'https://youtu.be/Li4j82QbBvk',
+        'https://youtu.be/PGJKeESLBpQ',
+        'https://youtu.be/kZUPCB9533Y',
+        'https://youtu.be/XbGs_qK2PQA',
+        'https://youtu.be/boh92DrYEWs',
+        'https://youtu.be/MtN1YnoL46Q',
+        'https://youtu.be/J---aiyznGQ',
+        'https://youtu.be/MkE2lxkw00w',
+        'https://youtu.be/ZqzESY7SIqU',
+        'https://youtu.be/ns1SGo3WCF4',
+        'https://youtu.be/7iFXyLah2oQ',
+        'https://youtu.be/3c66w6fVqOI',
+        'https://youtu.be/6e6RK8o1fcs',
+        'https://youtu.be/-RGhdxsQMHA',
+        'https://youtu.be/DmgsKR1O00c',
+        'https://youtu.be/FHviwdECNjU'
+    ];
+
+    function randomSite() {
+        var i = parseInt(Math.random() * sites.length);
+        location.href = sites[i];
+    };
